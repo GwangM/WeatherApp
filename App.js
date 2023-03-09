78d963c9a57db34782e60cab3fb7828e7f4b8337
rhkdaud268@gmail.com <rhkdaud268@gmail.com>
2023-03-10 01:50:06 +0900

    modify styles

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ViewBase } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
+      <StatusBar style='light'></StatusBar>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"skyblue"
   },
-});
+  city:{
+    flex:1.2,
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  cityName:{
+    fontSize:68,
+    fontWeight:"500"
+  },
+  weather:{
+    flex:3,
+  },
+  day:{
+    flex:1,
+    
+    alignItems:"center",
+
+  },
+  temp:{fontSize:178,
+    marginTop:50,
+  },
+  description:{
+    marginTop:-30,
+    fontSize:60
+  }
+})
